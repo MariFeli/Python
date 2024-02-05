4b6c7d688cc3d907d1f6b282ee5cd07c0eee48e5
MariFeli <158455000+MariFeli@users.noreply.github.com>
2024-02-05 18:07:33 +0100

    Add files via upload

---
+++ test4.py
@@ -0,0 +1,99 @@
+# x = 9.65
+# y=x
+# #y = 1.32
+# print(id(x))
+# print(id(y))
+
+# x = 10
+# print(id(x))
+# y= 10
+# print(id(y))
+
+# x = ["hola", 4, 5]
+# print(isinstance(x,int))
+
+# # print(type(x))
+# # print(id(x))
+# # x[0]= "Agur"
+# # print(id(x))
+
+# x = input("Introducir una cifra")
+# if isinstance (x,int):
+#     x*2
+# else:
+#     print("No es lo esperado")
+
+#Confirmar que la variable inflación es un tipo float e inmutable.
+    
+# inflacion = 5.7654
+# print(id(inflacion))
+# print(type(inflacion))
+# print(isinstance(inflacion,int))
+
+ #imprimir la suma de todos que son del tipo int (número entero). La respuesta en este caso es 15.
+
+# x = [5 , 4.32 , 10]
+# suma=0
+# for i in x:
+#     if isinstance (i,int):
+#         suma += i
+
+
+# print(suma)
+
+# i = 0
+# while i<10:
+#     print("Hola" + str(i))
+#     i=i+1
+# print("End")
+
+
+# i = 1
+# while i < 10:
+#     print (i)
+#     i += 1
+
+#Imprimir los valores desde 50 hasta 100
+# x = 50
+# while x < 100:
+#     print(x)
+#     x += 1
+
+#Desde 5, imprimir los valores 5 a 20, pero excluye 12.
+
+y = 5
+while y < 20:
+    #if y==12:
+    #continue      
+    print(y)
+    y = y+1
+
+
+#Preguntar al usuario por números hasta que el usuario 
+#introduzca “q” para quit. Sumar los valores y imprimir el resultado final.
+    
+#x = input("Introducir un valor: 'q' para quit ")
+
+
+    
+
+
+
+    
+
+    
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
