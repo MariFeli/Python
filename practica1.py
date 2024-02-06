62b928007a3c94b59dafdf56a01f542840b94758
MariFeli <158455000+MariFeli@users.noreply.github.com>
2024-02-06 12:15:05 +0100

    Create practica1.py

---
+++ practica1.py
@@ -0,0 +1,17 @@
+#Preguntar al usuario por números hasta que el usuario 
+#introduzca “q” para quit. Sumar los valores y imprimir el resultado final.
+    
+def sumarNumeros():
+    suma = 0
+    while True: #cuando necesitas crear un bucle infinito hasta que necesitas una razón para salir.
+        numero = input("Por favor ingresa un número o 'q' para salir: ")
+        if numero.lower() == 'q': #lower método de las cadenas que permite convertir los caracteres a minúscula.
+            break #Terminamos el bucle infinito
+        try:
+            suma += float(numero)
+        except ValueError: #Manejar cualquier error que pueda ocurrir durante la conversión del némero ingresado
+            print("Por favor ingresa un número válido o 'q' para salir.")
+    return suma
+
+resultado = sumarNumeros()
+print("La suma de los números ingresados es:", resultado)
